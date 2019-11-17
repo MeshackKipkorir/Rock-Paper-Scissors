fdbf4fd08e830a3bd3ef0a436866eb5ee89d7e55
Meshack Kipkorir <meshackkipkorir105@gmail.com>
2019-11-17 04:16:29 -0500

    created options list and message dictionary
    options list holds the players options while the message dictionary holds the message to be displayed for any outcome

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ rock ,paper, scissors game
 """
 
 from random import randint
+
+options = ["ROCK","PAPER","SCISSORS"]
+message = {
+  "key" : "Yawn it's  a tie",
+  "won" : "Yay you have won",
+  "lost" : "Aww you lost !"
+}
